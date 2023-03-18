4d9570373ff10f45601eec27d8102f3c249ec602
naeme <verygoodnaeme@protonmail.com>
2023-03-18 01:12:27 -0400

    switched to alacritty and added flameshot keybind

--- gaps/config.h
+++ gaps/config.h
@@ -69,13 +69,15 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *firefoxcmd[] = {"firefox", NULL};
-static const char *rangercmd[] = {"kittyranger", NULL};
+static const char *rangercmd[] = {"alacritty", "-e", "ranger", NULL};
+static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,			XK_p,	   spawn,	   {.v = flameshotcmd}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
