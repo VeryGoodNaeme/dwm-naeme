63ab8c8f1fefe1d022db7342e9b73ea1765fe79e
VeryGoodNaeme <127005284+VeryGoodNaeme@users.noreply.github.com>
2023-05-16 15:15:01 -0400

    Remove Font Awesome

--- no-gaps/config.h
+++ no-gaps/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=9"};
+static const char *fonts[]          = { "monospace:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#002b36";
 static const char col_gray2[]       = "#002b36";
